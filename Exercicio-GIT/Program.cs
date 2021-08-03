2cb6887843326e51e7f2a210a5920ad62f652a83
Leandro Aguiar <leandro.aguiar@sm2ce.com.br>
2021-08-03 16:24:40 -0300

    Projeto iniciado, olá mundo

---
+++ Exercicio-GIT/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Exercicio_GIT {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ola mundão vida loka!");
+            Console.WriteLine();
+        }
+    }
+}
