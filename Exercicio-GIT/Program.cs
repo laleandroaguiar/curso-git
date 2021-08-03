d51efd981ca2f17b89b4e8bb555f428a44805b1a
Leandro Aguiar <leandro.aguiar@sm2ce.com.br>
2021-08-03 17:21:55 -0300

    For de repetição

--- Exercicio-GIT/Program.cs
+++ Exercicio-GIT/Program.cs
@@ -6,6 +6,20 @@ namespace Exercicio_GIT {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Ola mundão vida loka!");
             Console.WriteLine();
+
+            Console.WriteLine("Profexo Chegou");
+
+            Console.WriteLine("Pedrinho BH");
+
+            for (int i = 0; i < 30; i ++) {
+                Console.WriteLine("oi");
+
+            }
+            
+
+            
+
         }
     }
 }
+
