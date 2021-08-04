31486f2354d798e14427adc038f2f7ef3c28ebcf
Leandro Aguiar <laleandroaguiar@gmail.com>
2021-08-04 11:19:55 -0300

    Incluindo traços para organizar as mensagens

--- Exercicio-GIT/Program.cs
+++ Exercicio-GIT/Program.cs
@@ -3,15 +3,15 @@
 namespace Exercicio_GIT {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("=========================================");
             Console.WriteLine("Hello World!");
             Console.WriteLine("VIDA DE PROGRAMADOR");
             Console.WriteLine();
-
+            Console.WriteLine("=========================================");
             Console.WriteLine("Curso UDEMY");
-
             Console.WriteLine("C#");
-
             Console.WriteLine("Teste de atualização no git e github");
+            Console.WriteLine("=========================================");
 
             for (int i = 0; i < 30; i ++) {
                 Console.WriteLine("oi");
